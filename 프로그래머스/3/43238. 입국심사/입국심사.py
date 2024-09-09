7b69a41d48333b6f34c4b2434161235bb605ede9
Daniel_Kim <60537388+kikiru328@users.noreply.github.com>
2024-09-09 23:13:47 +0900

    Update 입국심사.py

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,28 +1,25 @@
 def solution(n, times):
-	# 가능한 최솟값과 최댓값을 left와 right로 설정
-    left = 1
-    right = max(times) * n
-    
-    # 이분탐색이니 left가 right 이하인 동안
+    answer = 0
+    # right는 가장 비효율적으로 심사했을 때 걸리는 시간
+    # 가장 긴 심사시간이 소요되는 심사관에게 n 명 모두 심사받는 경우이다.
+    left = min(times)
+    right = max(times)*n
     while left <= right:
-    	# 가운데 : 더하고 2로 나눈 몫(정수)
-        mid = (left+right)//2
-        # 심사한 사람 수
-        people = 0
-     	
+        mid = (left+ right) // 2
+        checked = 0
         for time in times:
-        	# 해당 심사대에서 주어진 시간동안 심사 받은 수 더하기
-            people += mid//time
-            
-            # 중간에라도 이미 n명보다 많이 심사했다면 break
-            if people >= n:
+            # people 은 모든 심사관들이 mid분 동안 심사한 사람의 수
+            checked += mid // time
+            # 모든 심사관을 거치지 않아도 mid분 동안 n명 이상의 심사를 할 수 있다면 반복문을 나간다.
+            if checked >= n:
                 break
-        # n명 초과 심사했다면, 시간이 너무 많은 것
-        # 딱 n명 심사했더라도, 시간이 남을 가능성 있음
-        if people >= n:
+        
+        # 심사한 사람의 수가 심사 받아야할 사람의 수(n)보다 많거나 같은 경우
+        if checked >= n:
             answer = mid
-            right = mid -1
-        # n명 미만 심사했다면, 시간이 너무 부족하다
-        else :
+            right = mid - 1
+        # 심사한 사람의 수가 심사 받아야할 사람의 수(n)보다 적은 경우
+        elif checked < n:
             left = mid + 1
+            
     return answer
